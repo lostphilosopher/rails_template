93a19ed70ad0a5ef566402bbb5b35fd91b2c5481
lostphilosopher <wandersen02@gmail.com>
2017-03-31 16:46:01 -0500

    copy_file vs run

--- template.rb
+++ template.rb
@@ -21,6 +21,10 @@ end
 console_puts('Running bundle install.')
 run "bundle install"
 
+def source_paths
+  [File.expand_path(File.dirname(__FILE__))]
+end
+
 after_bundle do
   console_puts('Installing RSpec.')
   generate "rspec:install"
@@ -31,8 +35,8 @@ after_bundle do
 
   console_puts('Converting assets for Bootstrap.')
 	run "rm app/assets/stylesheets/application.css"
-  run "cp application.scss app/assets/stylesheets/application.scss"
-  run "cp application.js app/assets/javascript/application.js"
+  copy_file 'application.scss' 'app/assets/stylesheets/application.scss'
+  copy_file 'application.js' 'app/assets/javascript/application.js'
 
   console_puts('Creating the database.')
   rails_command "db:create"
